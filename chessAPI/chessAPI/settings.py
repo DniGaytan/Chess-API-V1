e459e014c372282ed1383d64f82263623d7ccca6
Daniel Gaytan <dnigaytan07@gmail.com>
2023-12-30 06:56:52 -0600

    Updated some stuff

--- chessAPI/chessAPI/settings.py
+++ chessAPI/chessAPI/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'ba8@6sc__sy6g%jr0fj&bdkhxm0o^wd4umd0srwmk0w6b$slo2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ec2-3-80-83-155.compute-1.amazonaws.com']
 
 
 # Application definition
@@ -122,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
